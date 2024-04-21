b489d650e9fe1d2ad6d379837a0d873155e47664
kianamay <maykiana@msu.edu>
2024-04-21 15:20:29 -0400

    integrate geo location api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '38fdc3c67fmsh84ac2bde703904cp10ddc8jsn897a9e0c3381',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
