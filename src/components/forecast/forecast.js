dbf2f00601bd995ad2a4755ae3b7816522be37b8
kianamay <maykiana@msu.edu>
2024-04-21 22:00:21 -0400

    my custom icons

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -26,7 +26,7 @@ const Forecast = ({ data }) => {
                   <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
                   <label className="day">{forecastDays[idx]}</label>
                   <label className="description">{item.weather[0].description}</label>
-                  <label className="min-max">{Math.round(item.main.temp_max)}°C /{Math.round(item.main.temp_min)}°C</label>
+                  <label className="min-max">{Math.round(item.main.temp_max)}°F /{Math.round(item.main.temp_min)}°F</label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
